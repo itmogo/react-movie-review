d634db07dd20e7f87b3ee9358acbb829e7306d0b
Edward Frempong <asamoah.frempong@gmail.com>
2021-05-20 04:18:17 +0000

    updates

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import MovieReviewClass from './MovieReviewClass';
 import MovieReviewFunction from './MovieReviewFunction';
